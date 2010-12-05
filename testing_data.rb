da4fc251201bd3a4350d89dfd23752faccef5ec4
Administrator <rice@ubuntu.(none)>
2010-12-05 23:14:20 +0800

    123

--- testing_data.rb
+++ testing_data.rb
@@ -1,13 +1,3 @@
-class TD < Thor
-  desc "testing_data", "生成测试数据"
-  def testing_data
-    puts "------重建数据库------"
-    %x{ rake db:reset }
-    puts "------生成测试数据----"
-
-    load_env
-
-    Department.create(:name => "IE")
     Department.create(:name => "老板")
     Department.create(:name => "采购")
     Department.create(:name => "仓库")
@@ -24,11 +14,6 @@ class TD < Thor
     User.create(:password_confirmation =>"hzuhzu", :password => "hzuhzu", :login => "admin", :email => "admin@gmail.com", 
       :role_id => 1, :name => '管理员', :department_id => 1)  # important!
 
-    User.create(:password_confirmation =>"hzuhzu", :password => "hzuhzu", :login => "elky"    , :email => "e@gmail.com", 
-      :role_id => 2, :name => 'Elky', :department_id => 2)
-    User.create(:password_confirmation =>"hzuhzu", :password => "hzuhzu", :login => "lucy", :email => "em@gmail.com", 
-      :role_id => 3, :name => 'Lucy', :department_id => 3)
-
 
     ActiveRecord::Base.connection.execute('truncate permissions;')
     Permission::Records.each do |r|
@@ -41,13 +26,3 @@ class TD < Thor
     Role.create(:title => "manager"   , :permission_ids => (1..41).to_a)
     Role.create(:title => "boss"   , :permission_ids => (1..41).to_a)
     
-    
-    puts "------完成------------"
-  end
-
-  private
-    def load_env
-      require 'config/environment'
-    end
-end
-
